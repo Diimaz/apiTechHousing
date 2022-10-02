922146d3990f5019e1873aebdc01259e9a6ee085
Diimaz <dimas98459@gmail.com>
2022-10-02 09:45:58 -0600

    update

--- app/Controllers/Usuarios.php
+++ app/Controllers/Usuarios.php
@@ -20,47 +20,74 @@ class Usuarios extends BaseController
     }
 
     public function create(){
-        $rules = service('validation');
-        $rules->setRules([
-            'nombre'=>'required|alpha_space',
-            'apellido'=>'required|alpha_space',
-            'telefono'=>'required|numeric|is_unique[tbl_usuarios.telefono]',
-            'email'=>'required|valid_email|is_unique[tbl_usuarios.email]',
-            'password'=>'matches[c-password]',
-        ],[
-            'nombre' => [
-                    'required' => 'Digite un nombre',
-                    'alpha_space' => 'Caracteres no permitidos'
-            ],
-            'apellido' => [
-                'required' => 'Digite un apellido',
-                'alpha_space' => 'Caracteres no permitidos'
-            ],
-            'telefono' => [
-                'required' => 'Digite un número de telefono',
-                'numeric' => 'Solo digite numeros',
-                'is_unique' => 'Este número de telefono ya existe'
-            ],
-            'email' => [
-                'required' => 'Digite un correo',
-                'valid_email' => 'Correo no valido',
-                'is_unique' => 'Este correo ya existe'
-            ],
-            'password' => [
-                'matches' => 'Las contraseñas no coinciden',
-            ],
-        ]);
-
-        if(!$rules->withRequest($this->request)->run()){
-            return $this->getResponse($rules->getErrors(), ResponseInterface::HTTP_BAD_REQUEST);
+        if(!isset($_COOKIE['idRol'])){
+            return $this->getResponse([
+                'message' => 'Error'
+            ]);
         }
+        if(!isset($_COOKIE['idUsuario'])){
+            return $this->getResponse([
+                'message' => 'Error'
+            ]);
+        }
+        if(!isset($_COOKIE['idDepartamento'])){
+            return $this->getResponse([
+                'message' => 'Error'
+            ]);
+        }
+        if($_COOKIE['idRol'] == null){
+            return $this->getResponse([
+                'message' => 'Error'
+            ]);
+        }
+        
+        if($_COOKIE['idRol'] == '1'){
+            $rules = service('validation');
+            $rules->setRules([
+                'nombre'=>'required|alpha_space',
+                'apellido'=>'required|alpha_space',
+                'telefono'=>'required|numeric|is_unique[tbl_usuarios.telefono]',
+                'email'=>'required|valid_email|is_unique[tbl_usuarios.email]',
+                'password'=>'matches[c-password]',
+            ],[
+                'nombre' => [
+                        'required' => 'Digite un nombre',
+                        'alpha_space' => 'Caracteres no permitidos'
+                ],
+                'apellido' => [
+                    'required' => 'Digite un apellido',
+                    'alpha_space' => 'Caracteres no permitidos'
+                ],
+                'telefono' => [
+                    'required' => 'Digite un número de telefono',
+                    'numeric' => 'Solo digite numeros',
+                    'is_unique' => 'Este número de telefono ya existe'
+                ],
+                'email' => [
+                    'required' => 'Digite un correo',
+                    'valid_email' => 'Correo no valido',
+                    'is_unique' => 'Este correo ya existe'
+                ],
+                'password' => [
+                    'matches' => 'Las contraseñas no coinciden',
+                ],
+            ]);
 
-        $input = $this->getRequestInput($this->request);
+            if(!$rules->withRequest($this->request)->run()){
+                return $this->getResponse($rules->getErrors(), ResponseInterface::HTTP_BAD_REQUEST);
+            }
 
-        $userModel = new UserModel();
-        $userModel->save($input);
+            $input = $this->getRequestInput($this->request);
 
-        return $this->getJWTForUser($input['email'], ResponseInterface::HTTP_CREATED);
+            $userModel = new UserModel();
+            $userModel->save($input);
+
+            return $this->getJWTForUser($input['email'], ResponseInterface::HTTP_CREATED);
+        }else{
+            return $this->getResponse([
+                'message' => 'No tiene privilegios',
+            ]);
+        }
     }
 
     private function getJWTForUser(string $email, int $responseCode = ResponseInterface::HTTP_OK){
